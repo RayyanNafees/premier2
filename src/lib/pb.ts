5d877a5bfa793ed109544d571a04a32a6eb3ff0f
RayyanNafees <nafees.rayyan@gmail.com>
2023-10-23 21:18:09 +0530

    Pagination logic added

--- src/lib/pb.ts
+++ src/lib/pb.ts
@@ -1,3 +1,10 @@
 import Pocketbase from 'pocketbase'
 const pb = new Pocketbase('https://premier.pockethost.io')
 export default pb
+
+export const filecdn = (
+  collection_id: string,
+  record_id: string,
+  filename: string
+) =>
+  `https://premier.pockethost.io/api/files/${collection_id}/${record_id}/${filename}`
---
+++ src/utils/range.ts
@@ -0,0 +1,1 @@
+export default (n: number) => new Array(n).fill(0).map((m, i) => i)
