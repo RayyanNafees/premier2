d8539ca84d12c46320bd56184da70db15d2ec135
RayyanNafees <nafees.rayyan@gmail.com>
2023-11-06 07:56:53 +0530

    Hyperlinks Fixed

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,4 +1,9 @@
 import type { Collection } from '@/components/products/types'
-import {atom} from 'nanostores'
+import { atom, computed } from 'nanostores'
 
 export const $cols = atom<Collection[]>([])
+
+export const $col_ids = computed(
+  $cols,
+  (cols) => new Set(cols.map((col: Collection) => col.name))
+)
