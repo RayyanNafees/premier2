0e51507d9497829c53493d0b30798d487e077c4d
RayyanNafees <nafees.rayyan@gmail.com>
2023-11-16 19:05:51 +0530

    Removed Extra filters from ProductLayout

--- src/utils/transforms.ts
+++ src/utils/transforms.ts
@@ -1,11 +1,11 @@
-export const capitalize_col = (colname: string) =>
+export const capitalize_col = (colname: string) => // WOODEN_FRAMES => Wooden Frames
   colname
     .split('_')
     .map((i) => i[0].toUpperCase() + i.slice(1).toLowerCase())
     .join(' ')
     .trim()
 
-export const colify = (capitalized_col: string) =>
+export const colify = (capitalized_col: string) => // Wooden Frames => WOODEN_FRAMES
   capitalized_col
     .split(' ')
     .map((i) => i.toUpperCase())
